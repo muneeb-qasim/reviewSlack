7d8a3ab5188eff009567ded42c2e9b73fbee4cc6
muneeb <muneebbutt5962@gmail.com>
2021-01-02 13:07:13 +0500

    Slack Web

--- server.js
+++ server.js
@@ -3,27 +3,28 @@ const app = express();
 const mongoose = require('mongoose');
 const cors = require('cors');
 const port = process.env.PORT || 5000;
-const {
-    MONGOURI
-} = require('./config/key');
+const {MONGOURI} = require('./config/key');
 
 app.use(cors());
-mongoose.connect(MONGOURI, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useUnifiedTopology: true,
+mongoose.connect(process.env.MONGODB_URI || MONGOURI, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
 });
 
 mongoose.connection.on('connected', () => {
-    console.log('Connected to MongoDB...');
+  console.log('Connected to MongoDB...');
 });
 
 mongoose.connection.on('err', () => {
-    console.log('Could not connect to MongoDB...', err);
+  console.log('Could not connect to MongoDB...', err);
 });
 
-
 app.use(express.json());
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+}
 
 app.use(require('./routes/user'));
 app.use(require('./routes/appReviews'));
